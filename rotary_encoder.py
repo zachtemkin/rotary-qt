48e1e69a2049a59f7a9b89a2fc4308982529df75
Zachary Temkin <zachtemkin@gmail.com>
2025-08-31 11:03:44 -0400

    Adds boot script to enable usb cdc

--- rotary_encoder.py
+++ rotary_encoder.py
@@ -1,7 +1,6 @@
+# code.py (CircuitPython on QT Py ESP32-S2)
 import time, json
-import board, busio
-import usb_cdc
-
+import board, busio, usb_cdc
 from adafruit_seesaw.seesaw import Seesaw
 from adafruit_seesaw import rotaryio, digitalio
 
@@ -9,28 +8,25 @@ i2c = busio.I2C(board.SCL, board.SDA)
 ss = Seesaw(i2c, addr=0x36)
 
 enc = rotaryio.IncrementalEncoder(ss)
-btn = digitalio.DigitalIO(ss, 24)        # built-in push button on Adafruit board
+btn = digitalio.DigitalIO(ss, 24)
 btn.switch_to_input(pull=digitalio.Pull.UP)
 
+ser = usb_cdc.data
 last = enc.position
-ser = usb_cdc.data    # shows up as /dev/ttyACM* on the Pi
 
 def emit(ev, **kw):
-    msg = {"t": time.monotonic(), "ev": ev, **kw}
+    msg = {"t": round(time.monotonic(), 3), "ev": ev, **kw}
     ser.write((json.dumps(msg) + "\n").encode("utf-8"))
 
-emit("boot", pos=last)
+emit("boot", raw=last, detents=last // 4)
 
 while True:
     pos = enc.position
     if pos != last:
-        # Many Seesaw encoders = 4 ticks per detent
-        detents = pos // 4
-        delta = pos - last
-        emit("turn", raw=pos, detents=detents, delta=delta)
+        emit("turn", raw=pos, delta=pos - last, detents=pos // 4)
         last = pos
 
-    if not btn.value:  # active-low
+    if not btn.value:
         emit("press")
         while not btn.value:
             time.sleep(0.01)
